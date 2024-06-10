c3c426ebf8f012bb7b216dff9a986b617bb6bebf
Dongmin Kim <ddmkim94@gmail.com>
2024-06-10 17:52:12 +0900

    Test: 회원가입 테스트 작성
    - 회원가입 비즈니스 로직 성공 테스트 작성

---
+++ src/test/java/com/dmk/mutbooks/domain/member/service/MemberServiceTest.java
@@ -0,0 +1,59 @@
+package com.dmk.mutbooks.domain.member.service;
+
+import com.dmk.mutbooks.domain.member.dto.request.JoinRequest;
+import com.dmk.mutbooks.domain.member.model.Member;
+import com.dmk.mutbooks.domain.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+
+@DisplayName("비즈니스 로직 테스트 - 회원")
+@ExtendWith(MockitoExtension.class)
+class MemberServiceTest {
+
+    @InjectMocks
+    private MemberService sut;
+
+    @Mock
+    private MemberRepository memberRepository;
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
+
+    @DisplayName("회원 정보를 입력하면, 회원가입이 완료된다.")
+    @Test
+    void givenMemberInfo_whenJoining_thenSaveMember() {
+        // given
+        JoinRequest joinRequest = createJoinRequest();
+        given(memberRepository.findByUsername(joinRequest.getUsername())).willReturn(Optional.empty());
+        given(passwordEncoder.encode(anyString())).willReturn("encrypt_password");
+        given(memberRepository.save(any(Member.class))).willReturn(mock(Member.class));
+
+        // when
+        sut.join(joinRequest);
+
+        // then
+        then(memberRepository).should().findByUsername(joinRequest.getUsername());
+        then(passwordEncoder).should().encode(anyString());
+        then(memberRepository).should().save(any(Member.class));
+    }
+
+    private JoinRequest createJoinRequest() {
+        return new JoinRequest("user1", "asdf1234", "user1@email.com");
+    }
+
+}
